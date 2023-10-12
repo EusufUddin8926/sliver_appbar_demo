21aa3f55b7a79a52496e14080d73119e1dfc9a72
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-10-13 00:10:30 +0600

    sliverappbar add with listtile

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,51 @@ class _MyAppState extends State<MyApp> {
         body: CustomScrollView(
           slivers: [
             SliverAppBar(
+              pinned: true,
+              floating: false,
               expandedHeight: 200,
               backgroundColor: Colors.red ,
-
-            )
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text("Sliver AppBar Demo"),
+              ),
+            ),
+            SliverList(delegate: SliverChildListDelegate([
+              addDetails("One", "This is one"),
+              addDetails("Two", "This is Two"),
+              addDetails("Three", "This is Three"),
+              addDetails("Four", "This is Four"),
+              addDetails("Five", "This is Five"),
+              addDetails("Six", "This is Six"),
+              addDetails("Seven", "This is Seven"),
+              addDetails("Eight", "This is Eight"),
+              addDetails("Nine", "This is Nine"),
+              addDetails("Ten", "This is Ten"),
+              addDetails("One", "This is one"),
+              addDetails("Two", "This is Two"),
+              addDetails("Three", "This is Three"),
+              addDetails("Four", "This is Four"),
+              addDetails("Five", "This is Five"),
+              addDetails("Six", "This is Six"),
+              addDetails("Seven", "This is Seven"),
+              addDetails("Eight", "This is Eight"),
+              addDetails("Nine", "This is Nine"),
+              addDetails("Ten", "This is Ten")
+            ]))
           ],
         ),
       ),
     );
   }
+}
+
+Widget addDetails(String title, String description){
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(description),
+      leading: CircleAvatar(
+        child: Text(title[0]),
+      ),
+    );
 }
 
 
